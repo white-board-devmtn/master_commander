2b06041a2eff8a43d9669be68bd715bfc4f9e6db
ChristianKamalu <ckamalu98@gmail.com>
2019-05-11 16:46:50 -0600

    fix database seed

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -3,9 +3,9 @@ CREATE TABLE Users (
 	firstName varchar(100) NOT NULL,
 	lastName varchar(100) NOT NULL,
 	email varchar(200) NOT NULL UNIQUE,
-	number int(11) NOT NULL,
-	img TEXT(2000) NOT NULL,
-	teacher bool NOT NULL DEFAULT 'false',
+	number integer NOT NULL,
+	img TEXT NOT NULL,
+	teacher BOOLEAN NOT NULL DEFAULT 'false',
 	hash TEXT NOT NULL,
 	CONSTRAINT Users_pk PRIMARY KEY (user_id)
 ) WITH (
@@ -16,7 +16,7 @@ CREATE TABLE Users (
 
 CREATE TABLE Class (
 	class_id serial NOT NULL,
-	course_id int NOT NULL,
+	course_id integer NOT NULL,
 	start_date varchar(50) NOT NULL,
 	end_date varchar(50) NOT NULL,
 	CONSTRAINT Class_pk PRIMARY KEY (class_id)
@@ -28,10 +28,10 @@ CREATE TABLE Class (
 
 CREATE TABLE Ass (
 	ass_id serial NOT NULL,
-	class_id int NOT NULL,
+	class_id integer NOT NULL,
 	ass_name varchar(300) NOT NULL,
 	ass_description varchar(1000) NOT NULL,
-	grade_possible int(3) NOT NULL,
+	grade_possible integer NOT NULL,
 	due_date varchar(50) NOT NULL,
 	CONSTRAINT Ass_pk PRIMARY KEY (ass_id)
 ) WITH (
@@ -42,7 +42,7 @@ CREATE TABLE Ass (
 
 CREATE TABLE Lecture (
 	lecture_id serial NOT NULL,
-	class_id int NOT NULL,
+	class_id integer NOT NULL,
 	lecture_date varchar(50) NOT NULL,
 	lecture_title varchar(200) NOT NULL,
 	lecture_description varchar(1000) NOT NULL,
@@ -56,10 +56,10 @@ CREATE TABLE Lecture (
 
 CREATE TABLE user_ass (
 	user_ass_id serial NOT NULL,
-	ass_id int NOT NULL,
-	user_id int NOT NULL,
+	ass_id integer NOT NULL,
+	user_id integer NOT NULL,
 	grade varchar(2),
-	complete bool NOT NULL DEFAULT 'false',
+	complete BOOLEAN NOT NULL DEFAULT 'false',
 	ass_link TEXT NOT NULL,
 	CONSTRAINT user_ass_pk PRIMARY KEY (user_ass_id)
 ) WITH (
@@ -70,7 +70,7 @@ CREATE TABLE user_ass (
 
 CREATE TABLE Events (
 	event_id serial NOT NULL UNIQUE,
-	user_id int NOT NULL,
+	user_id integer NOT NULL,
 	event_description varchar(1000) NOT NULL,
 	event_title varchar(200) NOT NULL,
 	event_date varchar(50) NOT NULL,
@@ -83,8 +83,8 @@ CREATE TABLE Events (
 
 CREATE TABLE User_Class (
 	user_class_id serial NOT NULL,
-	user_id int NOT NULL,
-	class_id int NOT NULL,
+	user_id integer NOT NULL,
+	class_id integer NOT NULL,
 	CONSTRAINT User_Class_pk PRIMARY KEY (user_class_id)
 ) WITH (
   OIDS=FALSE
@@ -94,8 +94,8 @@ CREATE TABLE User_Class (
 
 CREATE TABLE Messages (
 	message_id serial NOT NULL,
-	user_id int NOT NULL,
-	class_id int NOT NULL,
+	user_id integer NOT NULL,
+	class_id integer NOT NULL,
 	message varchar(500) NOT NULL,
 	CONSTRAINT Messages_pk PRIMARY KEY (message_id)
 ) WITH (
