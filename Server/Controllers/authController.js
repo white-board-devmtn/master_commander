00229e94fe4f456e58f0cb62448843cc598d2551
Ryan <ryan.reese11@gmail.com>
2019-05-13 13:58:55 -0600

    updated profile

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -58,7 +58,7 @@ module.exports = {
     },
 
     getUser: (req, res) => {
-        if (req.session.user) res.status(200).send(req.session.user)
+        if (req.session.user) res.status(200).send({userData: req.session.user})
         else res.status(200).send('Please log in');
     },
 
