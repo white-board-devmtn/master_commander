804c4c50e67ab3aae9cc6181a7b0a59cb7c2b8e2
Ryan <ryan.reese11@gmail.com>
2019-05-13 21:33:29 -0600

    added image to navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import './NavBar.css';
 import noImage from '../../images/no-image.png'
-import masterChief from '../../images/masterchief.jpg';
-import { logOutUser } from '../../Redux/Ducks/userReducer'
+import { logOutUser, getUser } from '../../Redux/Ducks/userReducer'
 
 
 const NavBar = (props) => {
@@ -15,10 +12,26 @@ const NavBar = (props) => {
     window.location.reload();
   }
 
-  return(
+  useEffect(() => {
+    getUser()
+  }, [])
+
+
+  return (
     <div className="navbar-container">
       <div className="navbar-option-container">
-        <img src={masterChief} alt="of person" className="navbar-profile-img"/>
+        <div>
+          {
+            props.user.img ? (
+              <img src={props.user.img} alt="of person" className="navbar-profile-img" />
+            ) : (
+                <div className="navbar-option-container">
+                  <i className="far fa-user navbar-font"></i>
+                  <span>Account</span>
+                </div>
+              )
+          }
+        </div>
         <Link to="/profile"><i className="fas fa-id-card navbar-font"></i></Link>
         <Link to="/dashboard"><i className="fas fa-desktop navbar-font"></i></Link>
         <Link to="/calendar"><i className="fas fa-calendar-alt navbar-font"></i></Link>
@@ -34,4 +47,4 @@ function mapStateToProps(reduxState) {
   }
 }
 
-export default connect(mapStateToProps, {logOutUser})(NavBar);
+export default connect(mapStateToProps, { logOutUser })(NavBar);
