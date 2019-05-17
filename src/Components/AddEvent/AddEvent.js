24bc12ee660d3bb8fe7d361b0704370ef0cbedaa
ChristianKamalu <ckamalu98@gmail.com>
2019-05-17 14:53:17 -0600

    styling

---
+++ src/Components/AddEvent/AddEvent.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import axios from 'axios';
+import './AddEvent.css'
+import 'react-datepicker/dist/react-datepicker.css'
+
+function AddEvent (props) {
+
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  const [title, setTitle] = useState('')
+
+
+  function createEvent() {
+    console.log('settevent', props)
+    const sDate = moment(startDate).format('YYYY/MM/DD, HH:mm')
+    const eDate = moment(endDate).format('YYYY/MM/DD, HH:mm')
+    axios.put('/api/addEvent', {eventTitle: title, startDate: sDate, endDate: eDate})
+    .catch(err => console.log(err))
+    props.getData();
+    props.toggleAddEvent(!props.addEvent);
+  }
+
+  return (
+    <div className={props.addEvent ? 'addEvent-component' : 'no-display'}>
+      <div className='empty-space' onClick={() => props.toggleAddEvent()}></div>
+      <div className='addEvent-container'>
+        <div className='addEvent-content'>
+          <div style={{marginBottom: '.5rem'}}>
+            <h4>Start Date:</h4>
+            <DatePicker 
+              selected={startDate}
+              onChange={setStartDate}
+              showTimeSelect
+              showYearDropdown
+              timeFormat="H:mm"
+              timeIntervals={15}
+              dateFormat="yyyy/MM/dd HH:mm"
+              timeCaption="time"
+            />
+          </div>
+          <div style={{marginBottom: '.5rem'}}>
+            <h4>End Date:</h4>
+            <DatePicker 
+              selected={endDate}
+              onChange={setEndDate}
+              showTimeSelect
+              showYearDropdown
+              timeFormat="H:mm"
+              timeIntervals={15}
+              dateFormat="yyyy/MM/dd HH:mm"
+              timeCaption="time"
+            />
+          </div>
+          <div style={{marginBottom: '.5rem'}}>
+            <h4>Event Title:</h4>
+            <textarea onChange={(event) => setTitle(event.target.value)} style={{width: '20rem'}}/>
+          </div>
+          <button className='create-event-button' onClick={createEvent}>Create Event</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddEvent
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -5,6 +5,8 @@ import './Profile.css'
 import NavBar from '../NavBar/NavBar'
 import axios from 'axios';
 
+import lake from '../../images/lake.jpg';
+
 import ProfileImage from './ProfileCard/ProfileImage'
 import withStyles from '@material-ui/core/styles/withStyles';
 
@@ -27,14 +29,15 @@ const Profile = (props) => {
 
   const { classes } = props;
 
-  const [firstName, updateFirstName] = useState('')
-  const [lastName, updateLastName] = useState('')
-  const [img, updateImg] = useState('')
-  const [password, updatePassword] = useState('')
-  const [phoneNumber, updatePhoneNumber] = useState('')
-  const [editToggle, updateEditToggle] = useState(true)
+  const [firstName, setFirstName] = useState(props.user.firstName)
+  const [lastName, setLastName] = useState(props.user.lastName)
+  const [email, setEmail] = useState(props.user.email)
+  const [img, setImg] = useState(props.user.img)
+  const [password, setPassword] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState(props.user.phoneNumber)
+  const [editToggle, setEditToggle] = useState(false)
 
-
+  console.log(firstName)
 
   useEffect(() => {
     props.getUser()
@@ -48,54 +51,35 @@ const Profile = (props) => {
     }).catch(err => console.log(err));
   };
 
-  console.log(props);
   return (
-    <>
+    <div className='profile-component'>
       <NavBar />
-      <div className="profileComponent">
-        <div className="profile-home-container">
-          {editToggle ? (
-            <div>
-              <ProfileImage
-                img={props.user.img}
-              />
-              <div className="profile-info-container">
-                <div className="profile-student-info">
-                  <h1>Your Information</h1>
-                  {/* <button style={{ cursor: "pointer" }} onClick={() => updateEditToggle(false)}>Edit Profile</button> */}
-                  <StudentInfo 
-                    email={props.user.email}
-                    firstName={props.user.firstName}
-                    lastName={props.user.lastName}
-                    number={props.user.phoneNumber}
-                  />
-                  
-                </div>
-
-                <div className="profile-student-info">
-                  <h1>Class Grades</h1>
-                </div>
-
-              </div>
-
-
-            </div>
-          ) : (
-              <div>
-                <input placeholder='First Name' value={firstName} onChange={e => updateFirstName(e.target.value)} />
-                <input placeholder='Last Name' value={lastName} onChange={e => updateLastName(e.target.value)} />
-                <input placeholder='Profile Image' value={img} onChange={e => updateImg(e.target.value)} />
-                <input placeholder='Phone Number' value={phoneNumber} onChange={e => updatePhoneNumber(e.target.value)} />
-
-
-                <button style={{ cursor: "pointer" }} onClick={() => { updateEditToggle(true); handleUpdateUser() }}>Edit Profile</button>
-                <button style={{ cursor: "pointer" }} onClick={() => updateEditToggle(true)}>Cancel</button>
-
-              </div>
-            )}
+      <div className='profile-component-container'>
+        <div style={{backgroundImage: `url(${lake})`}} className='profile-header'>
+          <div style={{backgroundImage: `url(${props.user.img})`}} className='profile-pic' />
+        </div>
+        <h1 className='user-name'>{props.user.firstName} {props.user.lastName}</h1>
+        <div className='info-display-container'>
+          <div className='info-containers'>
+            <p className={editToggle ? 'edit-profile' : 'no-display'} onClick={() => setEditToggle(false)}>confirm</p>
+            <p className={editToggle ? 'no-display' : 'edit-profile'} onClick={() => setEditToggle(true)}>edit profile</p>
+            <div className={editToggle ? 'user-info' : 'no-display'}><h3 style={{width: '50%'}}>First Name:</h3><input placeholder={props.user.firstName} value={firstName} onChange={e => setFirstName(e.target.value)}/></div>
+            <div className={editToggle ? 'user-info' : 'no-display'}><h3 style={{width: '50%'}}>Last Name:</h3><input placeholder={props.user.lastName} value={lastName} onChange={e => setLastName(e.target.value)}/></div>
+            <div className={editToggle ? 'no-display' : 'user-info'}><h3 style={{width: '50%'}}>Full Name:</h3><p>{props.user.firstName} {props.user.lastName}</p></div>
+            <div className={editToggle ? 'user-info' : 'no-display'}><h3 style={{width: '50%'}}>Email Address:</h3><input placeholder={props.user.email} value={email} onChange={e => setEmail(e.target.value)}/></div>
+            <div className={editToggle ? 'no-display' : 'user-info'}><h3 style={{width: '50%'}}>Email Address:</h3><p>{props.user.email}</p></div>
+            <div className={editToggle ? 'user-info' : 'no-display'}><h3 style={{width: '50%'}}>Phone Number:</h3><input placeholder={props.user.phoneNumber} value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/></div>
+            <div className={editToggle ? 'no-display' : 'user-info'}><h3 style={{width: '50%'}}>Phone Number:</h3><p>{props.user.phoneNumber}</p></div>
+            <div className={editToggle ? 'user-info' : 'no-display'}><h3 style={{width: '50%'}}>Profile Image:</h3><input placeholder={props.user.img} value={img} onChange={e => setImg(e.target.value)}/></div>
+            <div className='user-info' style={{borderBottom: '1px solid rgba(128, 128, 128, 0.466)'}}><h3 style={{width: '50%'}}>Password:</h3><p>change password</p></div>
+          </div>
+          <hr/>
+          <div className='info-containers'>
+            
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
